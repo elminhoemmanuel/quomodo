7840bbaba78214c3ecf77ba6af020dfb1ee9486f
elminhoemmanuel <amodue15@gmail.com>
2020-08-26 15:34:23 +0100

    initial uploads

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".check-btn").click(function(){
+        if ($(".details").val()==="") { 
+            window.alert("Please enter Employee Full name/Employee ID")
+        } else{
+            var x = document.getElementById("greeting");
+            x.style.visibility = "visible";
+            
+        };
+    })
+
+});
